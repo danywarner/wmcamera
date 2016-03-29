e06d6b462414f119cd595a7ef06e35a433d5070a
Daniel Warner <danywarner@gmail.com>
2016-03-29 14:30:48 -0500

    image from base64 unrotated after decoded

---
+++ WMcamera/CustomImageView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomImageView.swift
+//  WMcamera
+//
+//  Created by Daniel Warner on 3/29/16.
+//  Copyright © 2016 Daniel Warner. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageView: UIImageView {
+    
+}
--- WMcamera/ViewController.swift
+++ WMcamera/ViewController.swift
@@ -8,6 +8,49 @@
 
 import UIKit
 import MobileCoreServices
+extension UIImage {
+    public func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
+        let radiansToDegrees: (CGFloat) -> CGFloat = {
+            return $0 * (180.0 / CGFloat(M_PI))
+        }
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat(M_PI)
+        }
+        
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: CGPointZero, size: size))
+        let t = CGAffineTransformMakeRotation(degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        CGContextTranslateCTM(bitmap, rotatedSize.width / 2.0, rotatedSize.height / 2.0);
+        
+        //   // Rotate the image context
+        CGContextRotateCTM(bitmap, degreesToRadians(degrees));
+        
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+        
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+        
+        CGContextScaleCTM(bitmap, yFlip, -1.0)
+        CGContextDrawImage(bitmap, CGRectMake(-size.width / 2, -size.height / 2, size.width, size.height), CGImage)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -33,12 +76,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if mediaType == kUTTypeImage as String {
             let image = info[UIImagePickerControllerOriginalImage]
                 as! UIImage
+            let imageData = UIImagePNGRepresentation(image)
+            let base64String = imageData!.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+            let decodedData = NSData(base64EncodedString: base64String, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
             
-            imageView.image = image
+
+
+            print("hihih")
+            var img = UIImage(data: decodedData!)
+            if newMedia == true {
+                img = img?.imageRotatedByDegrees(90.0, flip: false)
+            }
+            
+            imageView.image = img
+            
             
             if (newMedia == true) {
                 UIImageWriteToSavedPhotosAlbum(image, self,
-                                               Selector("image:didFinishSavingWithError:contextInfo:"), nil)
+                                               #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
             } else if mediaType == kUTTypeMovie as String {
                 // Code to support video here
             }
